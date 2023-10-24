9f1bc24cb9cab4bbec60cd81faa7ca6e13343bd4
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-24 14:46:58 -0700

    Update QuestCommand.php

--- src/Terpz710/BlueGamesQuest/Command/QuestCommand.php
+++ src/Terpz710/BlueGamesQuest/Command/QuestCommand.php
@@ -7,21 +7,22 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use Terpz710\BlueGamesQuest\Form\QuestForm;
 use Terpz710\BlueGamesQuest\Main;
+use Terpz710\BlueGamesQuest\EventListener;
 
 class QuestCommand extends Command {
 
     private $plugin;
 
-    public function __construct(Main $plugin) {
+    public function __construct(Main $plugin, EventListener $eventListener) {
         parent::__construct("quest", "See the list of quests!", "/quest");
         $this->setPermission("quest.cmd");
         $this->plugin = $plugin;
+        $this->eventListener = $eventListener;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender instanceof Player) {
-            $eventListener = $this->plugin->getEventListener();
-            $questForm = new QuestForm($eventListener);
+            $questForm = new QuestForm($this->eventListener);
             $questForm->sendQuestList($sender, $this->plugin->getQuests());
         } else {
             $sender->sendMessage("This command can only be used in-game.");
