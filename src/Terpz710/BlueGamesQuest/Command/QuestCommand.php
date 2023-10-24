decb558d7b871ae21ad91525e60c2f9204e72905
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-23 21:54:38 -0700

    Update QuestCommand.php

--- src/Terpz710/BlueGamesQuest/Command/QuestCommand.php
+++ src/Terpz710/BlueGamesQuest/Command/QuestCommand.php
@@ -3,24 +3,22 @@
 namespace Terpz710\BlueGamesQuest\Command;
 
 use pocketmine\command\Command;
-use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use Terpz710\BlueGamesQuest\Form\QuestForm;
 use Terpz710\BlueGamesQuest\Main;
 
-class QuestCommand extends Command implements CommandExecutor {
+class QuestCommand extends Command {
 
     private $plugin;
 
     public function __construct(Main $plugin) {
-        parent::__construct("quest", "See the list of quest!", "/quest");
+        parent::__construct("quest", "See the list of quests!", "/quest");
         $this->setPermission("quest.cmd");
-        $this->setExecutor($this);
         $this->plugin = $plugin;
     }
 
-    public function onCommand(CommandSender $sender, string $commandLabel, array $args): bool {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender instanceof Player) {
             QuestForm::sendQuestList($sender, $this->plugin);
         } else {
