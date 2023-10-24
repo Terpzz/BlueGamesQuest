62389008f79d7e16549194182e0321527de5b1ec
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-23 22:57:19 -0700

    Update QuestForm.php

--- src/Terpz710/BlueGamesQuest/Form/QuestForm.php
+++ src/Terpz710/BlueGamesQuest/Form/QuestForm.php
@@ -5,15 +5,18 @@ namespace Terpz710\BlueGamesQuest\Form;
 use pocketmine\player\Player;
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\item\StringToItemParser;
-use Terpz710\BlueGamesQuest\Main;
 use Terpz710\BlueGamesQuest\EventListener;
 
 class QuestForm {
 
-    public static function sendQuestList(Player $player, Main $plugin) {
-        $quests = $plugin->getQuests();
+    private $eventListener;
 
-        $form = new SimpleForm(function (Player $player, $data) use ($quests, $plugin) {
+    public function __construct(EventListener $eventListener) {
+        $this->eventListener = $eventListener;
+    }
+
+    public function sendQuestList(Player $player, array $quests) {
+        $form = new SimpleForm(function (Player $player, $data) use ($quests) {
             if ($data === null) {
                 return;
             }
@@ -22,7 +25,7 @@ class QuestForm {
 
             if (isset($quests[$questName])) {
                 $questDetails = $quests[$questName];
-                self::sendQuestDetails($player, $questName, $questDetails, $plugin);
+                $this->sendQuestDetails($player, $questName, $questDetails);
             }
         });
 
@@ -36,19 +39,19 @@ class QuestForm {
         $form->sendToPlayer($player);
     }
 
-    public static function sendQuestDetails(Player $player, $questName, $questDetails, Main $plugin) {
+    public function sendQuestDetails(Player $player, $questName, $questDetails) {
         $description = $questDetails["description"];
         $reward = $questDetails["reward"];
         $requiredItem = $questDetails["required_item"];
 
-        $form = new SimpleForm(function (Player $player, $data) use ($questName, $requiredItem, $plugin) {
+        $form = new SimpleForm(function (Player $player, $data) use ($questName, $requiredItem) {
             if ($data === null) {
                 return;
             }
 
             if ($data === 0) {
-                if (self::hasRequiredItem($player, $requiredItem)) {
-                    self::handleQuestCompletion($player, $questName, $plugin);
+                if ($this->hasRequiredItem($player, $requiredItem)) {
+                    $this->handleQuestCompletion($player, $questName);
                 } else {
                     $player->sendMessage("You do not have the required items to claim this quest.");
                 }
@@ -63,7 +66,7 @@ class QuestForm {
         $form->sendToPlayer($player);
     }
 
-    private static function hasRequiredItem(Player $player, $requiredItem) {
+    private function hasRequiredItem(Player $player, $requiredItem) {
         if ($requiredItem !== null) {
             $parsedItem = StringToItemParser::getInstance()->parse($requiredItem);
             return $player->getInventory()->contains($parsedItem);
@@ -71,8 +74,8 @@ class QuestForm {
         return true;
     }
 
-    private static function handleQuestCompletion(Player $player, $questName, Main $plugin) {
+    private function handleQuestCompletion(Player $player, $questName) {
         $player->addTag("Quest: $questName");
-        $plugin->markQuestAsCompleted($player, $questName);
+        $this->eventListener->markQuestAsCompleted($player, $questName);
     }
 }
