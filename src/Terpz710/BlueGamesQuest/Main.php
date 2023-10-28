5e6fcd7ab4fa0d7aa6758ad883997c0a2ba8c07f
BlueGamesNetworkPMMP <140811591+Terpzz@users.noreply.github.com>
2023-10-28 12:44:52 -0700

    Update Main.php

--- src/Terpz710/BlueGamesQuest/Main.php
+++ src/Terpz710/BlueGamesQuest/Main.php
@@ -12,13 +12,16 @@ use Terpz710\BlueGamesQuest\Command\QuestCommand;
 class Main extends PluginBase implements Listener {
 
     private $questConfig;
+    private $completedQuestsConfig;
     private $eventListener;
 
     public function onEnable(): void {
         $this->saveResource("quest.yml");
         $this->questConfig = new Config($this->getDataFolder() . "quest.yml", Config::YAML);
 
-        $this->eventListener = new EventListener($this->questConfig);
+        $this->completedQuestsConfig = new Config($this->getDataFolder() . "completed_quests.yml", Config::YAML);
+
+        $this->eventListener = new EventListener($this->questConfig, $this->completedQuestsConfig);
 
         $this->getServer()->getPluginManager()->registerEvents($this->eventListener, $this);
         $this->getServer()->getCommandMap()->register("quest", new QuestCommand($this, $this->getEventListener()));
